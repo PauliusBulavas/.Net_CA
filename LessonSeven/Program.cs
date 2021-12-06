82a34de8ecd8ab12d26c64de892f3f4401f6c232
PauliusBulavas <94542478+PauliusBulavas@users.noreply.github.com>
2021-12-06 20:22:43 +0200

    added lesson Seven
    again

---
+++ LessonSeven/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LessonSeven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
