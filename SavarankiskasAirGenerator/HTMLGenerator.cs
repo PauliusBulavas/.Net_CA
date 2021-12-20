ba80d1bb88db528fa785970a86ad3ba436978ddb
PauliusBulavas <94542478+PauliusBulavas@users.noreply.github.com>
2021-12-20 22:32:52 +0200

    bandau lista isvest i console :/

---
+++ SavarankiskasAirGenerator/HTMLGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SavarankiskasAirGenerator
+{
+    public class HTMLGenerator
+    {
+        public void GenerateHTMLWithColor(List<ReportItem> report)
+        {
+            report.ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ SavarankiskasAirGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SavarankiskasAirGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mc = new ReportGenerator();
+
+            Console.WriteLine(report);
+        }
+    }
+}
