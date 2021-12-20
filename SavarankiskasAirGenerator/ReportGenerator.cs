776f209c2310a7cc8739b33f13371e13ae65bbe7
PauliusBulavas <94542478+PauliusBulavas@users.noreply.github.com>
2021-12-20 20:46:35 +0200

    working on reportGenerator

--- SavarankiskasAirGenerator/ReportGenerator.cs
+++ SavarankiskasAirGenerator/ReportGenerator.cs
@@ -4,11 +4,41 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace SavarankiskasAirGenerator
-{
+{ 
     public class ReportGenerator
     {
-        public ReportGenerator()
+
+        private AircraftModelRepository _aircraftModelRepository;
+        private AircraftRepository      _aircraftRepository;
+        private CompanyRepository       _companyRepository;
+        private CountryRepository       _countryRepository;
+
+        public ReportGenerator(AircraftModelRepository aircraftModelRepository,
+                               AircraftRepository aircraftRepository,
+                               CompanyRepository companyRepository,
+                               CountryRepository countryRepository)
         {
+            _aircraftModelRepository    =   aircraftModelRepository;
+            _aircraftRepository         =   aircraftRepository;
+            _companyRepository          =   companyRepository;
+            _countryRepository          =   countryRepository;
+        }
+
+        public List<ReportItem> GenerateReportAircraftInEurope()
+        {
+            List<Aircraft> aircrafts = _aircraftRepository.Retrieve();
+
+            foreach (var aircaft in aircrafts)
+            {
+                Company aircraftCompany         = _companyRepository.Retrieve(aircaft.CompanyId);
+                Country aircraftCountry         = _countryRepository.Retrieve(aircraftCompany.CountryId);
+                AircraftModel aircraftModel     = _aircraftModelRepository.Retrieve(aircraftCountry.Id);
+                if (aircraftModel == "Europe")
+                {
+
+                }
+
+            }
 
         }
     }
