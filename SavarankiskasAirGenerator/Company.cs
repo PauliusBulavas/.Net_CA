32a17f45c09fb70712438cf541feb9234d5a0477
PauliusBulavas <94542478+PauliusBulavas@users.noreply.github.com>
2021-12-20 18:45:50 +0200

    created models for flight generator

--- SavarankiskasAirGenerator/Company.cs
+++ SavarankiskasAirGenerator/Company.cs
@@ -6,5 +6,19 @@ namespace SavarankiskasAirGenerator
 {
     class Company
     {
+        public Company()
+        {
+
+        }
+
+        public Company(int id, string name, string countryId)
+        {
+            Id = id;
+            Name = name;
+            CountryId = countryId;
+        }
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string CountryId { get; set; }
     }
 }
---
+++ SavarankiskasAirGenerator/Country.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SavarankiskasAirGenerator
+{
+    class Country
+    {
+        public Country()
+        {
+
+        }
+
+        public Country(int id, string code, string name, string continent, bool belongsToEu)
+        {
+            Id = id;
+            Code = code;
+            Name = name;
+            Continent = continent;
+            BelongsToEu = belongsToEu;
+        }
+        public int Id { get; private set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Continent { get; set; }
+        public bool BelongsToEu { get; set; }
+    }
+}
+}
