bb4be7435cd850e772591d959afad37c384d70cf
PauliusBulavas <94542478+PauliusBulavas@users.noreply.github.com>
2021-12-20 18:41:09 +0200

    creating models for aircraft flight sender

---
+++ SavarankiskasAirGenerator/Aircraft.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SavarankiskasAirGenerator
+{
+    class Aircraft
+    {
+        public Aircraft()
+        {
+
+        }
+
+        public Aircraft(int id, string modelId, string companyId, string tailNumber)
+        {
+            Id = id;
+            ModelId = modelId;
+            CompanyId = companyId;
+            TailNumber = tailNumber;
+        }
+        public int Id { get; private set; }
+        public string ModelId { get; set; }
+        public string CompanyId { get; set; }
+        public string TailNumber { get; set; }
+    }
+}
+}
---
+++ SavarankiskasAirGenerator/MainTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SavarankiskasAirGenerator
+{
+    class MainTest
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
