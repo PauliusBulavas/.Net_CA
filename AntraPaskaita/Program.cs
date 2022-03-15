2dab4d7167da3976bcae16b62d3ff79e05bd27a0
PauliusBulavas <94542478+PauliusBulavas@users.noreply.github.com>
2022-03-15 18:55:02 +0200

    final upload

---
+++ AntraPaskaita/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace AntraPaskaita
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 uzduotis
+
+            //Console.WriteLine("input two numbers:");
+            //int x = int.Parse(Console.ReadLine());
+            //int y = int.Parse(Console.ReadLine());
+
+            //Console.Clear();
+
+            //Console.WriteLine($"pries\nx = {x}, y = {y}");
+
+            //int temp = x;
+            //x = y;
+            //y = temp;
+
+            //Console.WriteLine($"po\nx = {x}, y = {y}");
+
+            //2 - 3 uzduotis
+
+            //string nameOfSchool = "Code Academy";
+            //string nameOfCourse = ".NET_CA";
+            //int numberOfStudents = 19;
+            //string todaysDate = DateTime.Now.ToString("yyyy-MM-dd");
+            //DateTime courseStartDate = new DateTime(2022, 03, 01);
+            //DateTime courseEndDate = new DateTime(2022, 10, 21);
+
+            ////kad nebutu parasytas laikas salia datos
+            //string startDateString = courseStartDate.ToShortDateString();
+            //string endDateString = courseEndDate.ToShortDateString();
+
+            //int DaysBetween(DateTime startDate, DateTime endDate)
+            //{
+            //    TimeSpan span = endDate.Subtract(startDate);
+            //    return (int)span.TotalDays;
+            //}
+
+            //int totalDays = DaysBetween(courseStartDate, courseEndDate);
+
+            //int totalWeeks = totalDays / 7;
+            //int totalDaysWithoutWeekEnds = totalDays - totalWeeks * 3;
+
+            //Console.WriteLine($"Name of school - {nameOfSchool}");
+            //Console.WriteLine($"Name of course - {nameOfCourse}");
+            //Console.WriteLine($"Number of students - {numberOfStudents}");
+            //Console.WriteLine($"Todays date - {todaysDate}");
+            //Console.WriteLine($"Course start date - {startDateString}");
+            //Console.WriteLine($"Course end date - {endDateString}");
+            //Console.WriteLine($"total of work days - {totalDaysWithoutWeekEnds}");
+
+            ///////////////Kintamieji ir Operatoriai/////////////////////////////////
+            ///
+
+            //3 uzduotis
+
+            //Console.WriteLine("input two numbers to compare");
+            //int first = int.Parse(Console.ReadLine());
+            //int second = int.Parse(Console.ReadLine());
+
+            //if (first == second)
+            //{
+            //    Console.WriteLine($"{first} = {second}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{first} != {second}");
+            //}
+
+            //4 uzduotis
+
+            //Console.WriteLine("input two numbers");
+            //int first = int.Parse(Console.ReadLine());
+            //int second = int.Parse(Console.ReadLine());
+
+            //first++;
+            //second--;
+
+            //Console.WriteLine($"first number {first}\nsecond number {second}");
+
+            //5 uzduotis
+
+            //Console.WriteLine("input some text");
+            //string text = Console.ReadLine();
+
+            //if (text == "")
+            //{
+            //    Console.WriteLine("is text empty? - true");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("is text empty? - false");
+            //}
+
+            ////////////arba
+
+            //bool result = string.IsNullOrEmpty(text);
+            //Console.WriteLine($"string is empty: {result}");
+
+            //6 uzduotis
+
+            //Console.WriteLine("input a number");
+            //int number = int.Parse(Console.ReadLine());
+
+            //bool isMoreThanZero = number > 0;
+
+            //if (isMoreThanZero)
+            //{
+            //    Console.WriteLine("more than zero");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("less than zero");
+            //}
+
+            //7 uzduotis
+
+            //Console.WriteLine("input your name and surname:");
+            //string name = Console.ReadLine();
+            //string surname = Console.ReadLine();
+
+            //string nameToLower = name.ToLowerInvariant();
+            //string surnameToLower = surname.ToLowerInvariant();
+
+            //bool isEqual = nameToLower.Equals(surnameToLower);
+            //Console.WriteLine(isEqual);
+
+
+            //||, &&, !
+
+            //    if (skaicius || skaicius1 > 1) //or (arba)
+            //{
+
+            //}
+
+            //if (skaicius && skaicius > 1) //and (ir)
+            //{
+
+            //}
+
+            //if (skaicius !skaicius1) //not
+            //{
+
+            //}
+
+            //==, !=, >, <, <=, >=
+        }
+    }
+}
